2c292e6a12517dc46b28c144e97c2ebb5eba03d1
valeskaperezrretamal <jperezr@udd.cl>
2019-05-15 16:22:18 -0400

    se borran elementos de prueba y se agrega elementos NavigationMenu y SuperButton

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react'; 
-import logo from './logo.svg';
 import './App.css';
-import Title from './Title.js';
-import SubTitle from './SubTitle.js';
+//import MenuButton from './MenuButton.js';
+import NavigationMenu from './supercomponent/NavigationMenu.js';
+import SuperButton from './supercomponent/SuperButton.js';
+//import OptionMenu from './OptionMenu.js';
+
 
 function App() {
   return (
+
+
+
     <div className="App">
       <header className="App-header">
-         <Title name= "laboratoria"></Title>
-         <SubTitle name="proyecto"></SubTitle>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Burger Queen</h1>
+      <NavigationMenu></NavigationMenu>
+      <SuperButton></SuperButton>
+        
+        
+         
+        
+         
+       
       </header>
     </div>
   );
