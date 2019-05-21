a137cf850e67650d7a7667e4797177abf035e026
valeskaperezrretamal <jperezr@udd.cl>
2019-05-21 16:40:42 -0400

    se agregan botones con funcionalidad y contendio de cada boton

--- src/supercomponent/OptionMenuWaiter.js
+++ src/supercomponent/OptionMenuWaiter.js
@@ -1,17 +1,29 @@
 import React,{Component} from'react';
+import data from '../OptionMenu.json';
+
 class OptionMenuWaiter extends Component {
   constructor(props){
     super(props);
-    this.state= {data: JSON.parse(fetch('../OptionMenu.json'))};
     
-
+    this.state={category: "extra"};
+   
   }
+ 
   render(){
     return(
       <div>
-         {this.state.data.desayuno.map(optmenu=>{
-        return <p>{optmenu.option}</p>
-    })}
+        <button onClick={()=>{this.setState({category: "desayuno"})}}> Desayuno</button>
+        <button onClick={()=>{this.setState({category: "restodeldia"})}}> Resto del día</button>
+        <button onClick={()=>{this.setState({category: "tiposdecarne"})}}> Tipos de carne</button>
+        <button onClick={()=>{this.setState({category: "extra"})}}> extras</button>
+
+     
+    {this.state.category==="desayuno" ? data.desayuno.map(optmenu=>{return <p>{optmenu.option}</p>}):""}
+    {this.state.category==="restodeldia" ? data.restodeldia.map(optmenu=>{return <p>{optmenu.option}</p>}):""}
+    {this.state.category==="tiposdecarne" ? data.tiposdecarne.map(optmenu=>{return <p>{optmenu.option}</p>}):""}
+    {this.state.category==="extra" ? data.extra.map(optmenu=>{return <p>{optmenu.option}</p>}):""}
+    
+    
 
       </div>
     )
