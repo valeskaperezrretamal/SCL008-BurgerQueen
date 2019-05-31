c5e294601af63e9a9fa60c0eb77ed2a0a1c0ad4f
valeskaperezrretamal <jperezr@udd.cl>
2019-05-31 14:13:10 -0400

    Updates

---
+++ precache-manifest.1dd01fded220f722502e05647abf9239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56de7a601d3f9485c0c8dae72341688",
+    "url": "/SCL008-BurgerQueen/index.html"
+  },
+  {
+    "revision": "44ac2d6de8099142c646",
+    "url": "/SCL008-BurgerQueen/static/css/main.539f04fe.chunk.css"
+  },
+  {
+    "revision": "d7a7065c01f116427c89",
+    "url": "/SCL008-BurgerQueen/static/js/2.7926d9be.chunk.js"
+  },
+  {
+    "revision": "44ac2d6de8099142c646",
+    "url": "/SCL008-BurgerQueen/static/js/main.eb64d042.chunk.js"
+  },
+  {
+    "revision": "b518ceb1e546cef9b669",
+    "url": "/SCL008-BurgerQueen/static/js/runtime~main.d5fd1296.js"
+  }
+]);
