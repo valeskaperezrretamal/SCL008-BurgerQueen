25e0c851c49df15622aa7fe15776cd2ed7bbe3ec
valeskaperezrretamal <jperezr@udd.cl>
2019-06-11 15:36:22 -0400

    se arregla nombre de acciones a ocupar

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,6 +1,6 @@
 //puente entre nosotras y tras desarrolladoras, es para coordinar, es para saber que estados van haber en mi proyecto
 //las acciones que llaman estados, con mobre super claro, de la accioon que va a pasar
 //poner nombre en acciones
-export const Addname="Name";
-export const Order="Order";
-export const removeOrder="RemoveOrder";
+export const ADD_NAME="ADD_NAME";
+export const ADD_ORDER="ADD_ORDER";
+export const REMOVER_ORDER="REMOVER_ORDER";
