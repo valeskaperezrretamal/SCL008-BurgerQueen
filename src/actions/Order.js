7c8195eba9b058a66986a0d6e0d1603b2089772d
valeskaperezrretamal <jperezr@udd.cl>
2019-06-13 16:46:04 -0400

    se importan funciones (de acciones definidas en actiontypes) a ocupar y se crean funciones con estos nombres, función para agregar orden, para eliminar orden y para agregar el nombre

--- src/actions/Order.js
+++ src/actions/Order.js
@@ -1,11 +1,23 @@
-import {ADD_ORDER} from './actionTypes';
+import {ADD_TO_ORDER, REMOVE_OF_ORDER, ADD_NAME} from './actionTypes';
 
 
-export const addOrder = dispatch => item =>{
+export const addToOrder = dispatch => item =>{
     dispatch({
-        type: ADD_ORDER,
+        type: ADD_TO_ORDER,
         payload: item
     })
 
 }
+ export const removeOfOrder = dispatch => item =>{
+     dispatch({
+         type: REMOVE_OF_ORDER,
+         payload: item
+     })
+ }
 
+ export const addName = dispatch => item =>{
+    dispatch({
+        type: ADD_NAME,
+        payload: item
+    })
+}
